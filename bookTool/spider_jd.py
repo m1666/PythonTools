9a32a2a3d24ff138bd3bf48612c7fde0207351b2
maomin <160663534@qq.com>
2020-12-22 16:09:31 +0800

    爬取京东

--- bookTool/spider_jd.py
+++ bookTool/spider_jd.py
@@ -1,31 +1,44 @@
 import requests
 from lxml import html
 
-
 book_list = []
 
 
 def get_book_list(sn, page):
+    global book_list
     """ 爬取京东的图书数据 """
     url = 'https://search.jd.com/Search?keyword={sn}&page={page}'.format(sn=sn, page=page)
     # 获取html文档
-    header = {
-        'user-agent': 'Mozilla/5.0(Windows NT 10.0;Win64;x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'origin': 'http://search.jd.com',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Host': 'gia.jd.com',
-        'Proxy-Connection': 'keep-alive'
-    }
-    html_doc = requests.get(url, headers=header, verify=False).text
+    resp = requests.get(url)
+    resp.encoding = 'utf-8'
+    html_doc = resp.text
     # 获取xpath对象
     selector = html.fromstring(html_doc)
     # 获取图书列表集合
     ul_list = selector.xpath('//div[@id="J_goodsList"]/ul/li')
-    print(ul_list)
+    print(len(ul_list))
+    if len(ul_list) > 0:
+        book_list = book_list + ul_list
+        new_page = page + 1
+        get_book_list(sn, new_page)
+    else:
+        spider(book_list)
 
 
-# def spider():
+def spider(ul_list):
+    for li in ul_list:
+        # 标题
+        title = li.xpath('div/div[@class="p-name"]/a/@title')
+        print(title[0])
+        # 购买链接
+        link = li.xpath('div/div[@class="p-name"]/a/@title')
+        print(link[0])
+        # 价格
+        price = li.xpath('div/div[@class="p-price"]/strong/i/text()')
+        print(price[0])
+        # 店铺
+        store = li.xpath('div//a[@class="curr-shop"/@title]')
+        print(store[0])
 
 
 if __name__ == '__main__':
