61b1edf6e6957b849904af7392e3d4185290cb58
maomin <160663534@qq.com>
2020-12-22 17:17:29 +0800

    当当网数据爬取，修改当当网页面

---
+++ bookTool/spider_book.py
@@ -0,0 +1,26 @@
+from spider_dangdang import get_book_list as dangdang
+from spider_jd import get_book_list as jd
+from spider_taobao import spider as taobao
+import json
+
+
+def main(sn):
+    """ 图书比价工具 """
+    book_list = []
+    # 当当网图书
+    print('当当网数据爬取完成')
+    dangdang(sn, 1, book_list)
+
+    # 京东网图书
+    print('京东网数据爬取完成')
+    # 淘宝网图书
+    print('淘宝网数据爬取完成')
+    # 排序书籍列表
+    book_list = sorted(book_list, key=lambda item: float(item['price']), reverse=False)
+    for book in book_list:
+        print(book)
+
+
+if __name__ == '__main__':
+    sn = input('请输入ISBN：')
+    main(sn)
--- bookTool/spider_dangdang.py
+++ bookTool/spider_dangdang.py
@@ -4,26 +4,34 @@ from lxml import html
 book_list = []
 
 
-def spider(ul_list):
+def spider(ul_list, list=[]):
     for li in ul_list:
         # 标题
         title = li.xpath('a/@title')
-        print(title[0])
+        # print(title[0])
         # 购买链接
         link = li.xpath('a/@href')
-        print(link[0])
+        # print(link[0])
         # 价格
         price = li.xpath('p[@class="price"]/span[@class="search_now_price"]/text()')
-        print(price[0].replace('￥', ''))
+        # print(price[0])
+        price = '0.00' if len(price) == 0 else price[0].replace('¥', '')
+        # print(price)
         # 商家
         store = li.xpath('p[@class="search_shangjia"]/a/text()')
         store = '当当自营' if len(store) == 0 else store[0]
-        print(store)
+        list.append({
+            'title': title[0],
+            'price': price,
+            'link': link[0],
+            'store': store
+        })
+        # print(store)
 
-        print("---------------------------------")
+        # print("---------------------------------")
 
 
-def get_book_list(sn, page):
+def get_book_list(sn, page, bk_list = []):
     global book_list
     """ 爬取当当网的数据 """
     url = 'http://search.dangdang.com/?key={sn}&act=input&page_index={page}'.format(sn=sn, page=page)
@@ -38,12 +46,12 @@ def get_book_list(sn, page):
     ul_list = selector.xpath('//div[@id="search_nature_rg"]/ul/li')
 
     # print(len(ul_list))
-    if len(ul_list) > 0:
-        book_list = book_list + ul_list
-        new_page = page + 1
-        get_book_list(sn, new_page)
-    else:
-        spider(book_list),
+    # if len(ul_list) > 0:
+    #     book_list = book_list + ul_list
+    #     new_page = page + 1
+    #     get_book_list(sn, new_page)
+    # else:
+    spider(ul_list, bk_list)
 
 
 if __name__ == '__main__':
